21e699ae6e477ab560d6bcb2c0ca474b3bdd9945
Tim Mitra <tim@it-guy.com>
2023-10-10 12:35:08 -0400

    added Nav Bar button to add sample data

--- MoviesApp/Screens/MovieListScreen.swift
+++ MoviesApp/Screens/MovieListScreen.swift
@@ -10,13 +10,29 @@ import SwiftData
 
 struct MovieListScreen: View {
     
+    @Environment(\.modelContext) var modelContext
     // fetch all the movies
     @Query private var movies: [Movie]
     
     var body: some View {
-        List(movies) { movie in
-            Text(movie.title)
+        NavigationStack {
+            List(movies) { movie in
+                Text(movie.title)
+            }
         }
+        .toolbar {
+            Button("Add Movies", action: addSamples)
+        }
+    }
+    
+    func addSamples() {
+        let batman = Movie(title: "Bat Man", year: 1998)
+        let oppy = Movie(title: "Oppenheimer", year: 2023)
+        let starwars = Movie(title: "Star Wars", year: 1977)
+        
+        modelContext.insert(batman)
+        modelContext.insert(oppy)
+        modelContext.insert(starwars)
     }
 }
 

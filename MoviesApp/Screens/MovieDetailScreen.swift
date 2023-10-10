c14aaaff05769e2bb88e8119215f857c2c4332d3
Tim Mitra <tim@it-guy.com>
2023-10-10 18:11:47 -0400

    add context, add Review section and navigate with the movie

--- MoviesApp/Screens/MovieDetailScreen.swift
+++ MoviesApp/Screens/MovieDetailScreen.swift
@@ -15,6 +15,7 @@ struct MovieDetailScreen: View {
     
     @State private var title: String = ""
     @State private var year: Int?
+    @State private var showReviewScreen: Bool = false
     
     var body: some View {
         Form {
@@ -34,10 +35,33 @@ struct MovieDetailScreen: View {
                 }
             }.buttonStyle(.borderless) // no highlighting
             
+            Section("Reviews") {
+                Button(action: {
+                    showReviewScreen = true
+                }, label: {
+                    Image(systemName: "plus")
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                })
+                if let reviews = movie.reviews {
+                    if reviews.isEmpty {
+                        ContentUnavailableView {
+                            Text("No reviews")
+                        }
+                    } else {
+                        Text("list of reviews")
+                    }
+                }
+            }
+            
         }.onAppear {
             title = movie.title
             year = movie.year
         }
+        .sheet(isPresented: $showReviewScreen, content: {
+            NavigationStack {
+                AddReviewScreen(movie: movie) // passing in the movie
+            }
+        })
     }
 }
 

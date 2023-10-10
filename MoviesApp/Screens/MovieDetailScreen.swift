96e6194d614edf6472347b535e8f5e0ef3283efd
Tim Mitra <tim@it-guy.com>
2023-10-10 16:57:56 -0400

    created MovieDetailContainerScreen for the Preview, then used context to populate the Form with the movie

--- MoviesApp/Screens/MovieDetailScreen.swift
+++ MoviesApp/Screens/MovieDetailScreen.swift
@@ -6,13 +6,47 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct MovieDetailScreen: View {
+    
+    @Environment(\.modelContext) private var context
+    let movie: Movie
+    
+    @State private var title: String = ""
+    @State private var year: Int?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            TextField("title", text: $title)
+            TextField("year", value: $year, format: .number)
+        }.onAppear {
+            title = movie.title
+            year = movie.year
+        }
+    }
+}
+
+// this struct is for the PreviewProvider
+struct MovieDetailContainerScreen: View {
+    
+    @Environment(\.modelContext) private var context
+    @State private var movie:  Movie?
+    
+    var body: some View {
+        ZStack {
+            if let movie {
+                MovieDetailScreen(movie: movie)
+            }
+        }
+        .onAppear {
+            movie = Movie(title: "Spiderman", year: 2023)
+            context.insert(movie!)
+        }
     }
 }
 
 #Preview {
-    MovieDetailScreen()
+    MovieDetailContainerScreen()
+        .modelContainer(for: [Movie.self])
 }

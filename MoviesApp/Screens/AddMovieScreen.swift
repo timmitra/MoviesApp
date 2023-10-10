93456b33af889d36d193670d24bb153b1dca062c
Tim Mitra <tim@it-guy.com>
2023-10-10 12:51:48 -0400

    add title to Add Movie screen

--- MoviesApp/Screens/AddMovieScreen.swift
+++ MoviesApp/Screens/AddMovieScreen.swift
@@ -26,6 +26,7 @@ struct AddMovieScreen: View {
             TextField("Title", text: $title)
             TextField("Year", value: $year, format: .number)
         }
+        .navigationTitle("Add Movie")
         .toolbar {
             ToolbarItem(placement: .topBarLeading) {
                 Button("Close") {

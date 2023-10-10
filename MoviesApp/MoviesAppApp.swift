cc8aca9d7b58e2c0d1d170a43217a4c4b74ba13f
Tim Mitra <tim@it-guy.com>
2023-10-10 12:08:26 -0400

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Tim Mitra on 2023-10-10.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

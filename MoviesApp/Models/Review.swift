0801a572d9a62218e7e02aa0d8aec553fc03d85f
Tim Mitra <tim@it-guy.com>
2023-10-10 17:25:14 -0400

    create a Review

---
+++ MoviesApp/Models/Review.swift
@@ -0,0 +1,22 @@
+//
+//  Review.swift
+//  MoviesApp
+//
+//  Created by Tim Mitra on 2023-10-10.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Review {
+    var subject: String
+    var body: String
+    var movie: Movie?
+    
+    init(subject: String, body: String, movie: Movie? = nil) {
+        self.subject = subject
+        self.body = body
+        self.movie = movie
+    }
+}

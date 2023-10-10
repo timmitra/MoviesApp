f20e21389875cfbc3332da03d252d98937f79378
Tim Mitra <tim@it-guy.com>
2023-10-10 16:57:32 -0400

    created MovieDetailContainerScreen for the Preview, then used context to populate the Form with the movie

---
+++ MoviesApp/Screens/MovieDetailScreen.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailScreen.swift
+//  MoviesApp
+//
+//  Created by Tim Mitra on 2023-10-10.
+//
+
+import SwiftUI
+
+struct MovieDetailScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDetailScreen()
+}

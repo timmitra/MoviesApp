cce0ac8a134a4e7f43910ce8b0cce81d1875cc99
Tim Mitra <tim@it-guy.com>
2023-10-10 12:20:02 -0400

    add MovieListScreen with container and @Query

---
+++ MoviesApp/Screens/MovieListScreen.swift
@@ -0,0 +1,18 @@
+//
+//  MovieListScreen.swift
+//  MoviesApp
+//
+//  Created by Tim Mitra on 2023-10-10.
+//
+
+import SwiftUI
+
+struct MovieListScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieListScreen()
+}

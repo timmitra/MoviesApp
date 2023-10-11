f6887841eb41a275ee0724cba57f8daafe380199
Tim Mitra <tim@it-guy.com>
2023-10-10 21:47:16 -0400

    add deleteReview

--- MoviesApp/Views/ReviewListView.swift
+++ MoviesApp/Views/ReviewListView.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct ReviewListView: View {
     
+    @Environment(\.modelContext) private var context
     let reviews: [Review]
+    
+    private func deleteReview(indexSet: IndexSet) {
+        indexSet.forEach { index in
+            let review = reviews[index]
+            context.delete(review)
+            do {
+                try context.save()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
     
     var body: some View {
         List {
@@ -18,11 +31,12 @@ struct ReviewListView: View {
                     Text(review.subject)
                     Text(review.body)
                 }
-            }
+            }.onDelete(perform: deleteReview)
         }
     }
 }
 
 #Preview {
     ReviewListView(reviews: [])
+                    .modelContainer(for: [Review.self, Movie.self])
 }

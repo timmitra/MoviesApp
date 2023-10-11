296b30127c0931e081100e5895d5396637ecc43e
Tim Mitra <tim@it-guy.com>
2023-10-10 23:54:38 -0400

    add actor button, w/o separate view, presented 1/4 screen sheet, importing all models in Preview, adding context to insert the new actor

--- MoviesApp/Screens/MovieListScreen.swift
+++ MoviesApp/Screens/MovieListScreen.swift
@@ -10,13 +10,26 @@ import SwiftData
 
 struct MovieListScreen: View {
     
+    @Environment(\.modelContext) private var context
     // fetch all the movies
     @Query(sort: \Movie.title, order: .forward) private var movies: [Movie]
     @State private var isAddMoviePresented: Bool = false
+    @State private var isActorPresented: Bool = false
+    @State private var actorName: String = ""
     
+    private func saveActor() {
+        let actor = Actor(name: actorName)
+        context.insert(actor)
+    }
+
     var body: some View {
         MovieListView(movies: movies)
         .toolbar(content: {
+            ToolbarItem(placement: .topBarLeading) {
+                Button("Add Actor") {
+                    isActorPresented = true
+                }
+            }
             ToolbarItem(placement: .topBarTrailing) {
                 Button("Add Movie") {
                     isAddMoviePresented = true
@@ -28,10 +41,22 @@ struct MovieListScreen: View {
                 AddMovieScreen()
             }
         })
+        .sheet(isPresented: $isActorPresented, content: {
+            Text("Add Actor")
+                .font(.largeTitle)
+            TextField("Actor Name", text: $actorName)
+                .textFieldStyle(.roundedBorder)
+                .presentationDetents([.fraction(0.25)])
+                .padding()
+            Button("Save") {
+                isActorPresented = false
+                saveActor()
+            }
+        })
     }
 }
 
 #Preview {
     MovieListScreen()
-        .modelContainer(for: [Movie.self])
+        .modelContainer(for: [Movie.self, Review.self, Actor.self])
 }

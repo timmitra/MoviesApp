ec8274270927d5bfcff334937952637a3d9860ac
Tim Mitra <tim@it-guy.com>
2023-10-11 23:53:52 -0400

    consolidating the sheets

--- MoviesApp/Screens/MovieListScreen.swift
+++ MoviesApp/Screens/MovieListScreen.swift
@@ -8,6 +8,16 @@
 import SwiftUI
 import SwiftData
 
+enum Sheets: Identifiable {
+    case addMovie
+    case addActor
+    case showFiler
+    
+    var id: Int {
+        hashValue
+    }
+}
+
 struct MovieListScreen: View {
     
     @Environment(\.modelContext) private var context
@@ -15,9 +25,8 @@ struct MovieListScreen: View {
     @Query(sort: \Movie.title, order: .forward) private var movies: [Movie]
     @Query(sort: \Actor.name, order: .forward) private var actors: [Actor]
 
-    @State private var isAddMoviePresented: Bool = false
-    @State private var isActorPresented: Bool = false
     @State private var actorName: String = ""
+    @State private var activeSheet: Sheets?
     
     private func saveActor() {
         let actor = Actor(name: actorName)
@@ -38,31 +47,35 @@ struct MovieListScreen: View {
         .toolbar(content: {
             ToolbarItem(placement: .topBarLeading) {
                 Button("Add Actor") {
-                    isActorPresented = true
+                    activeSheet = .addActor
                 }
             }
             ToolbarItem(placement: .topBarTrailing) {
                 Button("Add Movie") {
-                    isAddMoviePresented = true
+                    activeSheet = .addMovie
                 }
             }
         })
-        .sheet(isPresented: $isAddMoviePresented, content: {
-            NavigationStack {
-                AddMovieScreen()
-            }
-        })
-        .sheet(isPresented: $isActorPresented, content: {
-            Text("Add Actor")
-                .font(.largeTitle)
-            TextField("Actor Name", text: $actorName)
-                .textFieldStyle(.roundedBorder)
-                .presentationDetents([.fraction(0.25)])
-                .padding()
-            Button("Save") {
-                isActorPresented = false
-                saveActor()
-                actorName = ""
+        .sheet(item: $activeSheet, content: { activeSheet in
+            switch activeSheet {
+            case .addMovie:
+                NavigationStack {
+                    AddMovieScreen()
+                }
+            case .addActor:
+                Text("Add Actor")
+                    .font(.largeTitle)
+                TextField("Actor Name", text: $actorName)
+                    .textFieldStyle(.roundedBorder)
+                    .presentationDetents([.fraction(0.25)])
+                    .padding()
+                Button("Save") {
+                    saveActor()
+                    actorName = ""
+                    self.activeSheet = nil // dismiss the sheet
+                }
+            case .showFiler:
+                Text("show filer screen")
             }
         })
     }
